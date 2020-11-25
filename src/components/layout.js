c0bf58ea50e24ca50b49bae3ddf05897677c51ca
ajay2507 <pajay2507@gmail.com>
2020-11-25 15:35:01 -0800

    add icon and update read me

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 import SideBar from "./sidebar.js"
+import icon from "../images/icon.png"
 import "./layout.css"
 
 const Layout = ({ children }) => (
@@ -8,14 +9,19 @@ const Layout = ({ children }) => (
     <Helmet
       title="Ajaykumar Prathap | Software Engineer"
       meta={[
-        { name: "description", content: "My Personal website" },
+        {
+          name: "description",
+          content: "Hi, this is Ajay. Software Engineer, Passionate about Web",
+        },
         {
           name: "keywords",
-          content: "software, Javascript, web, Ajaykumar, Ajay, full-stack",
+          content:
+            "Software Engineer, Javascript, Web developer, Full stack developer",
         },
       ]}
     >
       <html lang="en" />
+      <link rel="icon" href={icon}></link>
       <link
         href="https://fonts.googleapis.com/css?family=Google+Sans:300,400,500"
         rel="stylesheet"
